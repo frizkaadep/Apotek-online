5959f58affe1062f3799c3ca798f044038de3e65
Frizka Ade <frizkaadep@gmail.com>
2024-08-23 23:45:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pharma-ad
+ 
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $ownerRole = Role::create([
+            'name' => 'owner',
+        ]);
+
+        $buyerRole = Role::create([
+            'name' => 'buyer',
+        ]);
+
+        $user = User::create([
+            'name' => 'Frizka Ade',
+            'email' => 'frizkaade@example.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $user->assignRole($ownerRole);
+    }
+}
